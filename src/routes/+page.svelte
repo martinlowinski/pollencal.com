6cd4a305830511ccbb3f3734258eb669b676b9df
Martin Lowinski <martin@goldtopf.org>
2023-10-29 20:51:16 +0100

    Introduce sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	import Form from './Form.svelte';
+	import screenshot from '$lib/images/pollencal-screenshot.png';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+
+      <div class="px-4 pt-5 my-5 text-center border-bottom">
+        <h1 class="display-4 fw-bold text-body-emphasis">Pollen Forecast Webcal</h1>
+        <div class="col-lg-6 mx-auto">
+          <p class="lead mb-5">You can get the pollen forecast for your location directly into your calendar. This webcal uses emojis to display a 5 days forecast of various pollens. Enter your city, select pollens and adjust your preferences the get the feed. Works with all calendars supporting online .ics and emojis, like Google Calendar, Apple Calendar, Outlook or others.</p>
+        </div>
+        <div class="overflow-hidden" style="max-height: 30vh;">
+          <div class="container px-5">
+            <img src="{screenshot}" class="img-fluid border rounded-3 shadow-lg mb-4" alt="Example image" width="700" height="500" loading="lazy">
+          </div>
+        </div>
+      </div>
+
+	<Form />
+</section>
+
+<style>
+</style>
---
+++ src/routes/Form.svelte
@@ -0,0 +1,72 @@
+<script>
+	import { spring } from 'svelte/motion';
+
+	let count = 0;
+	let url = "webcal://pollencal.com/?location=Ditzingen&pollen=grass";
+
+	const displayed_count = spring();
+	$: displayed_count.set(count);
+	$: offset = modulo($displayed_count, 1);
+
+	/**
+	 * @param {number} n
+	 * @param {number} m
+	 */
+	function modulo(n, m) {
+		// handle negative numbers
+		return ((n % m) + m) % m;
+	}
+</script>
+
+
+<div class="px-4 pt-4 my-5">
+<div class="col-lg-6 mx-auto">
+  <form class="mb-3">
+    <div class="form-floating mb-3">
+      <input class="form-control form-control-lg" type="text" placeholder="Berlin" id="locationInput" aria-label="Your location">
+      <label for="locationInput">Your location</label>
+    </div>
+
+    <div class="d-flex flex-column flex-md-row p-4 gap-4 align-items-center justify-content-center">
+      <div class="list-group list-group-radio d-grid gap-2 border-0">
+	<div class="position-relative">
+	  <input class="form-check-input position-absolute top-50 end-0 me-3 fs-5" type="radio" name="listGroupRadioGrid" id="listGroupRadioGrid1" value="" checked>
+	  <label class="list-group-item py-3 pe-5" for="listGroupRadioGrid1">
+	    <strong class="fw-semibold">🌱 Grass</strong>
+	    <span class="d-block small opacity-75">There are hundreds of types of grasses, but only a few cause allergy symptoms.</span>
+	  </label>
+	</div>
+
+	<div class="position-relative">
+	  <input class="form-check-input position-absolute top-50 end-0 me-3 fs-5" type="radio" name="listGroupRadioGrid" id="listGroupRadioGrid2" value="">
+	  <label class="list-group-item py-3 pe-5" for="listGroupRadioGrid2">
+	    <strong class="fw-semibold">🌳 Tree</strong>
+	    <span class="d-block small opacity-75">Almond, birch, maple etc.</span>
+	  </label>
+	</div>
+
+	<div class="position-relative">
+	  <input class="form-check-input position-absolute top-50 end-0 me-3 fs-5" type="radio" name="listGroupRadioGrid" id="listGroupRadioGrid3" value="">
+	  <label class="list-group-item py-3 pe-5" for="listGroupRadioGrid3">
+	    <strong class="fw-semibold">🌼 Weed</strong>
+	    <span class="d-block small opacity-75">Ragweed, sagebrush etc.</span>
+	  </label>
+	</div>
+
+      </div>
+    </div>
+
+    <div class="d-grid gap-2">
+      <a target="_blank" rel="noopener" href="webcal://pollencal.com/?location=Ditzingen&pollen=grass" class="btn btn-primary btn-lg">Add to your Calendar</a>
+    </div>
+  </form>
+  <div class="bg-light text-center">
+    <pre><samp>
+    {url}
+    </samp></pre>
+  </div>
+</div>
+</div>
+
+<style>
+</style>
