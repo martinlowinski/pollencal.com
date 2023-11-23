a28702647737938ba15eb0da7921b8cc5da2327b
Martin Lowinski <martin.lowinski@metergrid.de>
2023-11-23 18:11:06 +0100

    Improve intro text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,7 @@
       <div class="px-4 pt-5 my-5 text-center border-bottom">
         <h1 class="display-4 fw-bold text-body-emphasis">Pollen Forecast Webcal</h1>
         <div class="col-lg-6 mx-auto">
-          <p class="lead mb-5">You can get the pollen forecast for your location directly into your calendar. This webcal uses emojis to display a 5 days forecast of various pollens. Enter your city, select pollens and adjust your preferences the get the feed. Works with all calendars supporting online .ics and emojis, like Google Calendar, Apple Calendar, Outlook or others.</p>
+          <p class="lead mb-5">Get the pollen forecast for your location directly into your calendar. This webcal uses emojis to display a 5 days forecast of various pollen indexes. Enter your city, select your pollen index and adjust your preferences the get the feed. Works with all calendars supporting online .ics and emojis, like Google Calendar, Apple Calendar, Outlook or others.</p>
         </div>
         <div class="overflow-hidden" style="max-height: 30vh;">
           <div class="container px-5">
