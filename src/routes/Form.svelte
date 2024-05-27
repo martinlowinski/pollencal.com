6746cb14c901f83f1e69d330c53dd93a1f01bcc9
Martin Lowinski <mail@martinlowinski.de>
2024-05-27 20:51:51 +0200

    Implement more safeguards for the process

--- src/routes/Form.svelte
+++ src/routes/Form.svelte
@@ -8,7 +8,7 @@
   import { ArrowRightOutline, CheckCircleSolid } from 'flowbite-svelte-icons';
   import PollenOption from '$lib/PollenOption.svelte';
 
-  const defaultPollen = 'graminales';
+  const defaultPollen = null;// = 'graminales';
 
   let getPosition = false;
   let coords = null;
@@ -38,7 +38,7 @@
     </div>
     <div class="row g-4 align-items-center">
       <div class="col-6">
-        <Button on:click="{() => (getPosition = true)}" class="font-semibold w-full text-lg" outline>
+        <Button on:click="{() => (getPosition = true)}" class="font-semibold w-full text-lg">
 	  Lookup location
           <span class:hidden={!loading}>
             <Spinner class="ms-2" size="4" />
@@ -126,13 +126,15 @@
       3. Subscribe to your individual calendar
     </div>
     <div class="">
-      <Button id="add" target="_blank" rel="noopener" href={webcalUrl} size="lg" class="font-semibold w-full text-lg" disabled={coords == null}>Add to your Calendar</Button>
+      <Button id="add" target="_blank" rel="noopener" href={webcalUrl} size="lg" class="font-semibold w-full text-lg" disabled={coords == null || selectedPollen == null}>Add to your Calendar</Button>
       {#if !coords}
         <Tooltip triggeredBy="[id='add']">Please lookup your location first</Tooltip>
+      {:else if !selectedPollen}
+        <Tooltip triggeredBy="[id='add']">Please select your pollen index first</Tooltip>
       {/if}
     </div>
   </form>
-  {#if coords}
+  {#if coords && selectedPollen}
   <span class="text-sm font-light text-black-900">If nothing happens, copy the following URL and subscribe manually with your calendar app:</span>
   <div class="overflow-x-auto py-4 px-2 font-mono text-sm bg-gray-100 text-center">
     {webcalUrl}
