dffd2bfb5b98f7bec53be7564ac433af55ada241
Martin Lowinski <mail@martinlowinski.de>
2024-06-01 13:30:49 +0200

    Improve intro markup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,15 +8,15 @@
 <title>Pollen Forecast Webcal</title>
 </svelte:head>
 
-<section class="container mx-auto py-3">
-  <div class="px-4 pt-5 my-5 text-center border-b">
-    <h1 class="text-5xl font-bold py-4">{$t('common.title')}</h1>
+<section class="container mx-auto py-4">
+  <div class="px-4 pt-5 my-4 text-center border-b">
+    <h1 class="text-5xl font-bold pb-2">{$t('common.title')}</h1>
     <div class="lg:w-2/3 mx-auto">
-      <p class="text-xl font-light text-gray-700 pt-6 pb-12">{$t('common.intro')}</p>
+      <h2 class="text-xl font-light text-gray-700 pt-6 pb-12">{$t('common.intro')}</h2>
     </div>
     <div class="w-full lg:w-3/4 mx-auto overflow-hidden" style="max-height: 30vh;">
       <div class="px-5">
-        <img src="{screenshot}" class="mx-auto" alt="Screenshot of calendar" loading="lazy">
+        <img src="{screenshot}" class="mx-auto" alt="Screenshot of subscribed calendar" loading="lazy">
       </div>
     </div>
   </div>
