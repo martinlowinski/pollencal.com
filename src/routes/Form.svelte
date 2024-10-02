0bee315f53a6082290d7faf666edc09cb2e0b925
Martin Lowinski <mail@martinlowinski.de>
2024-10-02 21:52:42 +0200

    Add rough draft of manual input for location

--- src/routes/Form.svelte
+++ src/routes/Form.svelte
@@ -142,6 +142,10 @@
               {#if !coords}
               <Tooltip>Please allow the browser to retrieve your location</Tooltip>
               {/if}
+              <div class="my-2">or</div>
+              <div class="mb-5">
+                <input type="text" id="location" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Stuttgart, Germany" />
+              </div>
               {#if coords}
               <div class="col-12" transition:fade={{ delay: 250, duration: 300 }}>
                 <span class="text-sm font-light text-black-900">Location found: {#if coords}{coords}{/if}</span>
