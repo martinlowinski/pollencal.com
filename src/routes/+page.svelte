9b062f6af7bc5de0f0241bf15d890099a0e43b3d
Martin Lowinski <mail@martinlowinski.de>
2025-05-02 16:19:17 +0200

    Fix grammar in alert

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,7 +17,7 @@
       <p class="mt-6 text-lg leading-8 text-gray-700">Get the pollen forecast for your location directly into your calendar. This webcal uses emojis to display a forecast of various pollen indexes for up to 5 days. Lookup your location and select your pollen index to get your individual calendar feed. Works with all calendars supporting online .ics and emojis, like Google Calendar, Apple Calendar, Outlook or others.</p>
       <Alert color="yellow" class="mt-4">
         <span class="font-medium">Attention:</span>
-        Due to the high demand of single users, this service is currently limited to ten requests per day. Please consider <a href="https://buymeacoffee.com/martinlowinski" target="_blank" class="underline underline-offset-4">donating</a> as my API costs increased by a factor of ~20 to keep this free.
+        Due to high demand from individual users, this service is currently limited to ten requests per day. Please consider <a href="https://buymeacoffee.com/martinlowinski" target="_blank" class="underline underline-offset-4">donating</a>, as the cost of my API has increased by a factor of ~20 in order to keep this service free.
       </Alert>
       <div class="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
         <a href="#setup" class="rounded-md bg-primary-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600">Get started</a>
